b041537e8b132007d802b851458829975ec8bb62
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-17 09:16:09 -0300

    Dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home/Home"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
