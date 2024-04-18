005ab2c97489d17ad74e0b97b076b3871ba9015b
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-18 11:00:20 -0300

    Ajustes en los cards

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom"
 import "./CountryCard.css"
 const CountryCard = ({ countryData }) => {
-    const { flags: { png: countryImage }, name: { common }, flags: { alt }, population, region, capital } = countryData
-
+    const { flags: { png: countryImage }, name: { official }, flags: { alt }, population, region, capital } = countryData
     return (
-        <Link className="country-card-anchor" to={`/${common}`}>
+        <Link className="country-card-anchor" to={`/${official}`}>
             <article className="country-card-container">
                 <img className="country-card-image" src={countryImage} alt={alt} />
-                <h2 className="country-card-title">{common}</h2>
-                {region && <h3>Region: {region} <span></span></h3>}
+                <h2 className="country-card-title">{official}</h2>
                 {population && <h3>Population: {population} <span></span></h3>}
+                {region && <h3>Region: {region} <span></span></h3>}
                 {capital && <h3>Capital: {capital} <span></span></h3>}
             </article>
         </Link>
