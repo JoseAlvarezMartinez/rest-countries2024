87fd075da3b032f288f24c7764bbdbe403d3dbc2
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-21 09:53:36 -0300

    Details of the countries

--- src/pages/CountryCardDetail/CountryCardDetail.jsx
+++ src/pages/CountryCardDetail/CountryCardDetail.jsx
@@ -18,8 +18,20 @@ const CountryCardDetail = () => {
   if (loading) return <CircularProgress color="inherit" />
   return (
     <div>
+      <img src={countryInformation.flags.png} alt="" />
+      <h2>{countryInformation.name.common}</h2>
+      <h3>Native Name: <span>{countryInformation.name.nativeName[Object.keys(countryInformation.name.nativeName)[0]].official}</span></h3>
+      <h3>Population: <span>{countryInformation.population}</span></h3>
+      <h3>Region: <span>{countryInformation.region}</span></h3>
+      <h3>Sub Region: <span>{countryInformation.subregion}</span></h3>
+      <h3>Capital: <span>{countryInformation.capital}</span></h3>
+      <h3>Top Level Domain: <span>{countryInformation.tld}</span></h3>
+      <h3>Currencies: <span>{countryInformation.currencies[Object.keys(countryInformation.currencies)[0]].name}</span></h3>
+      {/* {
+        countryInformation.languages.map(language =>  <h3>Idioma</h3>)
+      }
+      <h3>Languages: </h3> */}
 
-      {countryInformation.name.common}
     </div>
   )
 }
