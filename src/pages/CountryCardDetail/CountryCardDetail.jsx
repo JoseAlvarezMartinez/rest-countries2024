b3b16a15786977b92d3b2a4ddd1b867f80ff4c0a
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-20 21:42:43 -0300

    Loader Detail

--- src/pages/CountryCardDetail/CountryCardDetail.jsx
+++ src/pages/CountryCardDetail/CountryCardDetail.jsx
@@ -1,21 +1,24 @@
+import CircularProgress from '@mui/material/CircularProgress';
 import { useParams } from "react-router-dom"
 import { useCallApi, countryRender } from "../../customHooks/useCallApi"
 import { useState, useEffect } from "react"
 
 const CountryCardDetail = () => {
   const { countryName } = useParams()
+  const [loading, setLoading] = useState(true)
   const countriesData = useCallApi()
   const countryInformation = countryRender(countriesData, countryName)
-  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    if (countryInformation !== undefined) {
+    if (countriesData.length) {
       setLoading(false)
     }
-  }, [countryInformation])
+  }, [countriesData])
+
+  if (loading) return <CircularProgress color="inherit" />
   return (
     <div>
-      {loading ? "Cargando" : "Informacion"}
+      <p>{countryInformation.name.common}</p>
     </div>
   )
 }
